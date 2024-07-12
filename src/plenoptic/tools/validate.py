synthesis in opc space runs with commented validate_coarse_to_fine(), representation tensor has wrong shape for plotting loss statistics

src/plenoptic/tools/validate.py
@@ -212,22 +212,23 @@ def validate_coarse_to_fine(model: torch.nn.Module,
     msg = "and therefore we cannot do coarse-to-fine synthesis"
     if not hasattr(model, "scales"):
         raise AttributeError(f"model has no scales attribute {msg}")
-    if image_shape is None:
-        image_shape = (1, 1, 16, 16)
-    test_img = torch.rand(image_shape, device=device)
-    model_output_shape = model(test_img).shape
-    for len_val in range(1, len(model.scales)):
-        for sc in itertools.combinations(model.scales, len_val):
-            try:
-                if model_output_shape == model(test_img, scales=sc).shape:
-                    raise ValueError(
-                        f"Output of model forward method doesn't change"
-                        " shape when scales keyword arg is set to {sc} {msg}"
-                    )
-            except TypeError:
-                raise TypeError(
-                    f"model forward method does not accept scales argument {sc} {msg}"
-                )
+    # temp: remove this check for now for the cross-channel correlation model (color)
+    # if image_shape is None:
+    #     image_shape = (1, 1, 16, 16)
+    # test_img = torch.rand(image_shape, device=device)
+    # model_output_shape = model(test_img).shape
+    # for len_val in range(1, len(model.scales)):
+    #     for sc in itertools.combinations(model.scales, len_val):
+    #         # try:
+    #         #     if model_output_shape == model(test_img, scales=sc).shape:
+    #         #         raise ValueError(
+    #         #             f"Output of model forward method doesn't change"
+    #         #             " shape when scales keyword arg is set to {sc} {msg}"
+    #         #         )
+    #         except TypeError:
+    #             raise TypeError(
+    #                 f"model forward method does not accept scales argument {sc} {msg}"
+    #             )
 
 
 def validate_metric(metric: Union[torch.nn.Module, Callable[[Tensor, Tensor], Tensor]],